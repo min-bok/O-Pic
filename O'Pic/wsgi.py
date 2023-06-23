800c16c735cc8378e612375829c2c3cdbfae2fd0
min-bok <leehy0782@gmail.com>
2023-06-23 17:51:41 +0900

    feat: 고정요소 및 재사용 요소 구분 - head, header, footer 요소 구분 - accountapp templates 폴더 생성

--- O'Pic/wsgi.py
+++ O'Pic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for onion project.
+WSGI config for O'Pic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onion.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "O'Pic.settings")
 
 application = get_wsgi_application()
---
+++ accountApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accountapp.views import hello_world
+
+app_name = "accountapp"
+
+urlpatterns = [
+    path('hello/', hello_world, name="hello")
+]
