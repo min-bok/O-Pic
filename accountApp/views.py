8f3305b0b164ddeae017f6b9b696f7f368fcb6e8
min-bok <leehy0782@gmail.com>
2023-06-27 16:20:36 +0900

    feat: 회원가입 및 login/logout 구현 - createView 를 이용한 회원가입 구현 - LoginView 및 LogoutView를 이용한 로그인, 로그아웃 구현 - 로그인/로그아웃시 라우팅될 경로 설정     - next     - redirct_url 설정

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,5 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from accountapp.models import Hello
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def hello_world(req):
-    return render(req, "accountapp/hello.html")
+    if req.method == "POST":
+        temp = req.POST.get("hello_input")
+
+        new_hello = Hello()
+        new_hello.text = temp
+        new_hello.save()
+
+        global list
+        list = Hello.objects.all() # Hello의 모든 데이터를 긁어옴
+
+        return HttpResponseRedirect(reverse('accountapp:hello')) # account/hello로 리다이렉트
+    else:
+        return render(req, "accountapp/hello.html", context={"list": list}) 
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accounapp/hello.html')
+    template_name = "accountapp/create.html"
