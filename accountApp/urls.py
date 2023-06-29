3032c396e91f30968d2c2e7dafa05f3e431fcb8a
min-bok <leehy0782@gmail.com>
2023-06-29 17:57:10 +0900

    feat: detail page 연결 및 font 추가

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "accountapp"
@@ -8,5 +8,6 @@ urlpatterns = [
     path('hello/', hello_world, name="hello"),
     path('create/', AccountCreateView.as_view(), name="create"),
     path('login/', LoginView.as_view(template_name="accountapp/login.html"), name="login"),
-    path('logout/', LogoutView.as_view(), name="logout")
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name="detail")
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from accountapp.models import Hello
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -27,3 +27,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accounapp/hello.html')
     template_name = "accountapp/create.html"
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = "accountapp/detail.html"
